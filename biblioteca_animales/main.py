fdc13f3e8139de2a82b7261180c744c3e7706dbb
iChris998 <christopher.morales52@uabc.edu.mx>
2024-05-13 18:19:28 -0700

    se agrego una clase Perro

--- biblioteca_animales/main.py
+++ biblioteca_animales/main.py
@@ -1,8 +1,15 @@
 # __main__.py
 from gato import Gato
+from perro import Perro
 
 # Crear una instancia de Gato
 mi_gato = Gato("Felix")
 
 # Llamar al método hacer_sonido
 print(mi_gato.hacer_sonido())
+
+# Crear una instancia de Gato
+mi_perro = Perro("Bon")
+
+# Llamar al método hacer_sonido
+print(mi_perro.hacer_sonido())
